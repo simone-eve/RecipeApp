0da352d4c2be0f272fed6f7c394bf4d07684cdce
Simone Le Roux <st10215860@vcconnect.edu.za>
2023-04-25 05:50:23 +0000

    Created the main menu which will be used when the user needs to alter the recipe in any way. Also chose the names for each method and put them in the recipe.

--- RecipeApp/RecipeOptions.cs
+++ RecipeApp/RecipeOptions.cs
@@ -8,5 +8,42 @@ namespace RecipeApp
 {
     internal class RecipeOptions
     {
+        public void Menu()
+        {
+            Console.WriteLine("\nWould you like to: \n1) Enter the Recipe Details \n2) View the Recipe \n3) Scale your Recipe " +
+                "\n4) Reset Scale \n5) Delete Recipe \n6) Exit");
+            int menuChoice = Convert.ToInt32(Console.ReadLine());
+
+            switch (menuChoice)
+            {
+                case 1:
+                    RecipeDetails();
+                    break;
+
+                case 2:
+                    ViewRecipe();
+                    break;
+
+
+                case 3:
+                    ScaleRecipe();
+                    break;
+
+                case 4:
+                    ConvertToOriginalQuantity();
+                    break;
+
+                case 5:
+                    DeleteRecipe();
+                    break;
+
+                case 6:
+                    System.Environment.Exit(0);
+                    break;
+            }
+
+
+
+        }
     }
 }
