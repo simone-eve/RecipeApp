caa86eeccb0315d08950d8957c3e754fbcb5fb7b
Simone Le Roux <st10215860@vcconnect.edu.za>
2023-04-26 16:29:30 +0000

    Added the ConvertToOriginalQuantity method so that the users data can be converted back to the original in the case that the user wants the original quantities.

--- RecipeApp/RecipeOptions.cs
+++ RecipeApp/RecipeOptions.cs
@@ -8,6 +8,9 @@ namespace RecipeApp
 {
     internal class RecipeOptions
     {
+        int scaleNumber = 0;
+        int EndChoice = 0;
+
         public void Menu() //creating the Menu method which will allow the user to input details about their recipe along with select how they alter it.
         {
             int menuChoice = 0;
@@ -46,15 +49,15 @@ namespace RecipeApp
 
 
                 case 3:
-                   /// ScaleRecipe();
+                   ScaleRecipe();
                     break;
 
                 case 4:
-                   // ConvertToOriginalQuantity();
+                   ConvertToOriginalQuantity();
                     break;
 
                 case 5:
-                  //  DeleteRecipe();
+                  DeleteRecipe();
                     break;
 
                 case 6:
@@ -62,12 +65,8 @@ namespace RecipeApp
                     break;
             }
            
-
-
-
         }
 
-        int EndChoice = 0;
         public void RecipeDetails() //creating a RecipeDetails method so that the user can input all relevant details about their recipe.
         {
             double iQuantity = 0;
@@ -129,7 +128,7 @@ namespace RecipeApp
                         RecipeLists.stepList.Add(new Steps(stepDescription)); //adding each piece of information that the user inputs into the list.
                     }
 
-                    Console.WriteLine("Recipe added!");
+                    Console.WriteLine("\nRecipe details added!");
                 }
                 catch (FormatException)
                 {
@@ -172,7 +171,7 @@ namespace RecipeApp
         void ViewRecipe() //creating a ViewRecipe method so that all the data the user inputs can be displayed.
         {
             Console.ForegroundColor = ConsoleColor.Cyan;  //adding colour to text.
-            Console.WriteLine("\nIngredients: ");
+            Console.WriteLine("Ingredients: ");
             Console.ResetColor();
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("\n------------------------");  //creating a break to make the applicaation look more organised.
@@ -180,7 +179,7 @@ namespace RecipeApp
             foreach (var viewIngredient in RecipeLists.ingredientList) //using a foreach loop so that each ingredient in the list can be displayed.   //code attribution: C# Foreach: what it is, How it works, Syntax and Example Code, https://www.simplilearn.com/tutorials/asp-dot-net-tutorial/csharp-foreach#:~:text=The%20foreach%20loop%20in%20C%23%20uses%20the%20%27in%27%20keyword%20to,variable%20changes%20in%20every%20iteration.
             {
                 Console.WriteLine("Name: " + viewIngredient.Name + "\nQuantity: " + viewIngredient.Quantity
-                    + " " + viewIngredient.unitOfMeasurement);
+                    + " " + viewIngredient.unitOfMeasurement + "\n");
             }
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine("------------------------");
@@ -205,6 +204,7 @@ namespace RecipeApp
             {
                 try
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine("\n------------------------");  //creating a break to make the applicaation look more organised.
                     Console.ResetColor();
                     Console.WriteLine("1)Menu \n2)Exit");  //enabling the user to exit the program or go back to the main method so that they can alter their data how they choose.
@@ -230,23 +230,212 @@ namespace RecipeApp
 
         }
 
+        void ScaleRecipe() //creating a ScaleRecipe method so that the quantities of each ingredient can be scaled according to the users input.
+        {
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\nPlease select the number of the scaling choice: \n1) 0.5(half) \n2) 2(double) " +
+                "\n3) 3(Triple)");
+                    scaleNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nPlease enter a valid quantity.\n");
+                }
+            } while (scaleNumber == 0);
+            
+
+            switch (scaleNumber) //using a switch statement to correctly scale the quantities based on the users input.
+            {
+                case 1:
+                    foreach (var scaleIngridient in RecipeLists.ingredientList) //using a foreach loop so that each ingredient in the list can be scaled.
+                    {
+                        scaleIngridient.Quantity = scaleIngridient.Quantity / 2; //scaling the recipe.
+                    }
+                    Console.WriteLine("\nIngredients scaled!");
+                    break;
 
 
+                case 2:
+                    foreach (var scaleIngridient in RecipeLists.ingredientList)
+                    {
+                        scaleIngridient.Quantity = scaleIngridient.Quantity * 2;
+                    }
+                    Console.WriteLine("\nIngredients scaled!");
+                    break;
 
 
+                case 3:
+                    foreach (var scaleIngridient in RecipeLists.ingredientList)
+                    {
+                        scaleIngridient.Quantity = scaleIngridient.Quantity * 3;
+                    }
+                    Console.WriteLine("\nIngredients scaled!");
+                    break;
+            }
+
+            do //using a try-catch statement to ensure that the application does not fail if the user inputs the wrong data.
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("\n------------------------");  //creating a break to make the applicaation look more organised.
+                    Console.ResetColor();
+                    Console.WriteLine("1)Menu \n2)Exit");  //enabling the user to exit the program or go back to the main method so that they can alter their data how they choose.
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("------------------------");
+                    Console.ResetColor();
+
+                    EndChoice = Convert.ToInt32(Console.ReadLine());
+                    if (EndChoice.Equals(1)) //using an if statement to send the user to whichever choice they make.
+                    {
+                        Menu();
+                    }
+                    else
+                    {
+                        System.Environment.Exit(0);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nPlease enter a valid quantity.\n");
+                }
+            } while (EndChoice == 0);
+        }
+
+        void ConvertToOriginalQuantity() //creating a ConvertToOriginalQuantity method so that the quantities of each ingredient can be scaled back to the original input.
+        {
+            switch (scaleNumber)
+            {
+                case 1:
+                    foreach (var scaleIngredient in RecipeLists.ingredientList) //using a foreach loop so that each ingredient in the list can be de-scaled.
+                    {
+                        scaleIngredient.Quantity = scaleIngredient.Quantity * 2; //de-scaling the recipe.
+
+                    }
+                    Console.WriteLine("\nQuantity reverted.");
+                    break;
 
 
+                case 2:
+                    foreach (var scaleIngredient in RecipeLists.ingredientList)
+                    {
+                        scaleIngredient.Quantity = scaleIngredient.Quantity / 2;
+                    }
+                    Console.WriteLine("\nQuantity reverted.");
+                    break;
 
 
+                case 3:
+                    foreach (var scaleIngredient in RecipeLists.ingredientList)
+                    {
+                        scaleIngredient.Quantity = scaleIngredient.Quantity / 3;
+                    }
+                    Console.WriteLine("\nQuantity reverted.");
+                    break;
+            }
 
+            do //using a try-catch statement to ensure that the application does not fail if the user inputs the wrong data.
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("\n------------------------");  //creating a break to make the applicaation look more organised.
+                    Console.ResetColor();
+                    Console.WriteLine("1)Menu \n2)Exit");  //enabling the user to exit the program or go back to the main method so that they can alter their data how they choose.
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("------------------------");
+                    Console.ResetColor();
 
+                    EndChoice = Convert.ToInt32(Console.ReadLine());
+                    if (EndChoice.Equals(1)) //using an if statement to send the user to whichever choice they make.
+                    {
+                        Menu();
+                    }
+                    else
+                    {
+                        System.Environment.Exit(0);
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nPlease enter a valid quantity.\n");
+                }
+            } while (EndChoice == 0);
+        }
+    
 
+    void DeleteRecipe() //creating a DeleteRecipe method to clear the data inputed into the lists.
+    {
+      int deleteChoice = 0;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("\nPlease confirm you would like to delete your Recipe: \n1) Yes \n2) No"); //prompting the user whether they are sure they would like to delete the recipe.
+                    deleteChoice = Convert.ToInt32(Console.ReadLine());
 
+                    if (deleteChoice.Equals(1))
+                    {
+                        RecipeLists.ingredientList.Clear(); //clearing the lists       //code attribution: C# List.Clear() – Remove All Element from List, https://www.tutorialkart.com/c-sharp-tutorial/c-sharp-list-clear/
+                        RecipeLists.stepList.Clear();
+                        Console.WriteLine("\nRecipe Deleted!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nPlease enter a valid quantity.\n");
+                }
+            } while (deleteChoice == 0); 
 
+        do //using a try-catch statement to ensure that the application does not fail if the user inputs the wrong data.
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\n------------------------");  //creating a break to make the applicaation look more organised.
+                Console.ResetColor();
+                Console.WriteLine("1)Menu \n2)Exit");  //enabling the user to exit the program or go back to the main method so that they can alter their data how they choose.
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("------------------------");
+                Console.ResetColor();
 
-
-
-
-
+                EndChoice = Convert.ToInt32(Console.ReadLine());
+                if (EndChoice.Equals(1)) //using an if statement to send the user to whichever choice they make.
+                {
+                    Menu();
+                }
+                else
+                {
+                    System.Environment.Exit(0);
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("\nPlease enter a valid quantity.\n");
+            }
+        } while (EndChoice == 0);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 }
